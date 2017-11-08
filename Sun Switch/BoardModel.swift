1a0e07d985a05d0bb481c1603d3f22f1fafdf761
Zachary Brester <ZBrester@Gmail.com>
2017-11-07 22:51:51 -0800

    Pushing my current changes. Very loose, as I don't have anything that can check grammar, using Notepad++. Will try to clean up tomorrow.

--- Sun Switch/BoardModel.swift
+++ Sun Switch/BoardModel.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+class BoardModel: NSObject {
+	private let rows: Int = 5 // Number of starting Rows. The number of columns is determined in the RowModel
+	private var currentRows : Int = 5 // Current number of rows. This cannot exceed rows
+	private var board : [RowModel]()
+	private var diff : Int
+	private var validPieces : [PieceModel]()	//The roster of available pieces. This is based
+												//on the current level.
+	func init(difficulty: Int) {
+		diff = (difficulty <= 0 ) ? 1 : difficulty
+		super.init()
+		generatePieces(startLevel: diff)
+		generateBoard(startLevel: diff)
+	}
+	
+	func generatePieces(startLevel: Int) {
+		//This uses the difficulty level to select a number of piece types to add to the
+		//roster of available pieces. This will be pretty hard-coded, unfortunately.
+		
+	}
+	func generateBoard(startLevel: Int) {
+		//Later: Differentiate build based on starting levels
+		//This is used to create a new board, based on starting level.
+		
+		for i in 0..<rows {
+			board.append(createRow()) 
+		}
+		
+	}
+	
+	func newPiece() -> PieceModel{
+		//This should hopefully create a new piece based off the type chosen at random.
+		
+		let index = arc4random_uniform(validPieces.count)
+		let piece_type: PieceModel.Type = validPieces[index].self
+		return piece_type.init()
+	}
+	
+	func advanceLevel() {
+		//Advance board level.
+		//As the level progresses, this will unlock new available pieces, including potential 
+		//negative pieces (unbreakable blocks, etc)
+		
+		diff += 1
+		
+	}
+	
+	func restoreRow() {
+		let row = createRow()
+		board.append(row)
+		currentRows += 1
+		//Any other cleanup we might need.
+	}
+	
+	func createRow() -> RowModel{
+		let row = RowModel()
+		row.populate(pieceList: validPieces)
+		return row
+}
--- Sun Switch/GameModel.swift
+++ Sun Switch/GameModel.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+
+class GameModel: NSObject {
+	private var board : BoardModel!
+	private var level : Int	//The current level of difficulty, integer from 1 to an arbitrary amount
+	private var score : Int
+	private var nextGoal: Int
+	private var streak: Int
+	private var timeLeft : TimeInterval
+	
+	func init(start: Int) {
+		level = start
+		score = 0
+		super.init()
+		nextGoal = getNextGoal(current: start)
+		board = BoardModel(start)
+		resetTimer()
+	}
+	
+	func getNextGoal(current: Int) {
+		nextGoal = current * 1000
+		if( score >= nextGoal) {
+			advanceLevel() 
+		}
+	}
+	
+	func advanceLevel() {
+		let streakRestore: Int = 3
+		level += 1
+		board.advanceLevel()
+		getNextGoal()
+		
+		resetTimer()
+		
+		//Check current "restore row" count. If enough, we restore a new row.
+		if(streak >= streakRestore) {
+			if( board.missingRows() > 0) {
+				restoreRow()
+			}
+		}
+	}
+	
+	func resetTimer() {
+	//Set the timer based on the current level.
+	//Timer starts at a large amount and decreases each level, capping at a yet undetermined amount.
+	
+		let cap: Int = 10
+		let maxTimer : TimeInterval = 300
+		let minTimer : TimeInterval = 30
+		if( level >= cap) {
+			timeLeft = minTimer
+		}
+		else {
+			timeLeft = maxTimer - ( (level - 1) * 30)
+		}
+		
+		//Re-initialize the actual timer.
+	}
+	
+	func restoreRow() {
+		board.restoreRow()
+	}
+
+
+}
