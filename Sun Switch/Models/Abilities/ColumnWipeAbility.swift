a63679a41d4637a1a8cd853f38ed218dccfee12f
ZBrester <ZBrester@Gmail.com>
2017-12-11 19:07:48 -0800

    Fixed a bug on AudioPlayer causing songs to not loop. Added Resurrection and ColumnWipe ability. Fixed a few various bugs throughout. Added 3 new characters. Tweaked shop prices of characters and made characters appear sorted in the shop based on cost.

---
+++ Sun Switch/Models/Abilities/ColumnWipeAbility.swift
@@ -0,0 +1,9 @@
+//
+//  ColumnWipeAbility.swift
+//  Sun Switch
+//
+//  Created by student on 12/11/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import Foundation
---
+++ Sun Switch/Models/Abilities/RowRestoreAbility.swift
@@ -0,0 +1,9 @@
+//
+//  RowRestoreAbility.swift
+//  Sun Switch
+//
+//  Created by student on 12/11/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import Foundation
